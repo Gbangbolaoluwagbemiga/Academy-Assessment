bf90586daaad7560930c9d7dae1a89cf5f8ee2c9
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-09 19:02:33 +0100

    NavBar component completed

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+function NavBar() {
+  return (
+    <div>
+      <nav class="nav justify-content-center  ">
+        <a class="nav-link active" href="#" aria-current="page">
+          Active link
+        </a>
+        <a class="nav-link" href="#">
+          Link
+        </a>
+        <a class="nav-link disabled" href="#">
+          Disabled link
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
