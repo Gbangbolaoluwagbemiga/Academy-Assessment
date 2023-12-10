dc61df53a6c2b39588720ddcb873cec1786181f3
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-10 18:13:23 +0100

    Sub header style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import NavBar from './components/NavBar';
+import SubHeader from './components/SubHeader';
 
 function App() {
   return (
     <div className=" container">
       <NavBar />
+      <hr />
+      <SubHeader />
     </div>
   );
 }
