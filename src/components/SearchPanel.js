a943c3386701a01c1146d5b991eae3eb5e31ec7d
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-11 20:20:22 +0100

    search panel implementation

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -4,14 +4,14 @@ import Arrow from '../Images/SearchPanelArrow.png';
 function SearchPanel() {
   return (
     <div className="mx-auto">
-      <p className="text-center">
-        Search Among <span className="sub-text">58340 </span> Courses And <br />{' '}
-        Find Your Favorite Course
-      </p>
+      <h2 className="text-center search-header-text mb-5 ">
+        Search Among <span className="sub--text">58340 </span> Courses And{' '}
+        <br /> Find Your Favorite Course
+      </h2>
 
       <div className="search-box row">
-        <button className="btn-panel col-2">Categories</button>
-        <p className="col-7">
+        <button className="btn-panel btn-search col-2">Categories</button>
+        <p className="col-6">
           <input
             type="text"
             placeholder="Search Anything"
@@ -19,7 +19,7 @@ function SearchPanel() {
           />
           <img className="search-icon" src={searchIcon} alt="Search-icon" />
         </p>
-        <p className="col-2">Or View The Following Courses...</p>
+        <p className="col-3">Or View The Following Courses...</p>
         <p className="col-1">
           <img src={Arrow} alt="Arrow" />
         </p>
