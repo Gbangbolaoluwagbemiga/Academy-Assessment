4d53079f568cbc37b806d9e9240f271f92bb8ffa
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-11 22:19:11 +0100

    Axios installed

--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -1,15 +1,26 @@
+import {Axios} from 'axios';
 import DownWardIcon from '../Images/new-course icon.png';
 
-const initialState={
-    course:[]
+const initialState = {
+  course: [],
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'value':
 
+    default:
+      return state;
+  }
 }
-function reducer(){
-    switch
-}
-
 
 function NewMovie() {
+  function fetchCourse() {
+    Axios.get('https://freetestapi.com/api/v1/books').then(res =>
+      console.log(res)
+    );
+  }
+  fetchCourse();
+
   return (
     <div className="new-movies mt-5">
       <div className="new-movies-header row">
@@ -27,24 +38,23 @@ function NewMovie() {
       </div>
 
       <div className="course-content">
-        <img src={} alt="Course-content" className='col-12' />
+        {/* <img src={} alt="Course-content" className='col-12' /> */}
         <p className="course-title col-12"></p>
         <div className="features row">
-            <p className='col-4'>
-                <img src={} alt="Icon"  className='col-2'/>
-                <p className='col-6'>Lesson:</p>
-                <p className='col-6'>{}</p>
-            </p>
-            <p className='col-4'>
-                <img src={} alt="Icon"  className='col-2'/>
-                <p className='col-6'>Student:</p>
-                <p className='col-6'>{}</p>
-            </p>
-            <p className='col-4'>
-            <img src={} alt="Icon"  className='col-2'/>
-                <p className='col-6'>Student:</p>
-               
-            </p>
+          <p className="col-4">
+            {/* <img src={} alt="Icon"  className='col-2'/> */}
+            <p className="col-6">Lesson:</p>
+            <p className="col-6">{}</p>
+          </p>
+          <p className="col-4">
+            {/* <img src={} alt="Icon"  className='col-2'/> */}
+            <p className="col-6">Student:</p>
+            <p className="col-6">{}</p>
+          </p>
+          <p className="col-4">
+            {/* <img src={} alt="Icon"  className='col-2'/> */}
+            <p className="col-6">Student:</p>
+          </p>
         </div>
       </div>
     </div>
