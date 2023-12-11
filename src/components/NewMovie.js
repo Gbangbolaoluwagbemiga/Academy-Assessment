eb36c92057ec3ab3617fd809969fe42d9133d006
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-11 21:26:15 +0100

    New movie component implementation

--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -5,15 +5,25 @@ function NewMovie() {
     <div className="new-movies mt-5">
       <div className="new-movies-header row">
         <p className="Title-Section col-6">New Courses(BOOKS)</p>
-        <div className="tabs row col-6 ">
-          <p className="col-2 ms-auto">All Courses</p>
-          <p className="col-2 ms-auto">Design</p>
-          <p className="col-2 ">Development</p>
-          <p className="col-2 ms-auto">Photography</p>
-          <p className="col-3 ms-auto">
-            Music
-            <img src={DownWardIcon} alt="icon" className="ms-5 " />{' '}
+        <div className="tabs  col-6 ">
+          <p className="">All Courses</p>
+          <p className="">Design</p>
+          <p className="">Development</p>
+          <p className="">Photography</p>
+          <p className="">Music</p>
+          <p>
+            <img src={DownWardIcon} alt="icon" className=" " />{' '}
           </p>
+        </div>
+      </div>
+
+      <div className="course-content">
+        <img src={} alt="Course-content" className='col-12' />
+        <p className="course-title col-12"></p>
+        <div className="features row">
+            <p className='col-4'></p>
+            <p className='col-4'></p>
+            <p className='col-4'></p>
         </div>
       </div>
     </div>
