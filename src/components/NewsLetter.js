9f58f7b879dd4353691aa8141ead7f8d2b613e89
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-13 16:35:13 +0100

    Mobile responsiveness

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,12 +1,18 @@
 function NewsLetter() {
   return (
-    <div className="news-letter d-flex my-5 justify-content-between">
+    <div className="news-letter d-md-flex my-5 justify-content-between">
       <div className="letter-text ">
         Find Out About The Latest Courses With The{' '}
         <span className="sub--text"> Academy</span> Newsletter
       </div>
-      <form>
-        <input type="text" placeholder="Email Address..." />
+      <form className="news-form">
+        <p className="news-search">
+          <input
+            type="text"
+            placeholder="Email Address..."
+            className="searcher "
+          />
+        </p>
         <button className="btn-panel">Search</button>
       </form>
     </div>
