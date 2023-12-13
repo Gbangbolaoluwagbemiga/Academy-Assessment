bfbe5d6248d34b172b095ab305eddcfe030ab907
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-13 17:35:59 +0100

    Footer implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,28 @@
+import logo from '../Images/Academy.png';
+
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer row">
+      <div className="top-footer row">
+        <p className="text col-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas
+          purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris
+          rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed
+          euismod nisi porta lorem mollis. Morbi tristique senectus et netus.
+          Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien
+          faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper
+          velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius
+          duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam
+          maecenas sed enim. Velit ut tortor pretium
+        </p>
+        <p className="logo col-4">
+          <img src={logo} alt="logo" className="col-12" />
+          <p className="fw-bold text-logo">ACADEMY</p>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
