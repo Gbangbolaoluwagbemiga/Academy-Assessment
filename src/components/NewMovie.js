4457ee1562dd1103c44d6589305206f8c019ed2c
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-13 11:12:01 +0100

    New movies component completed

--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -4,13 +4,13 @@ function NewMovie({courses}) {
   return (
     <div className="new-movies row mt-5">
       <div className="new-movies-header row">
-        <p className="Title-Section col-6">New Courses(BOOKS)</p>
-        <div className="tabs  col-6 ">
-          <p className="">All Courses</p>
-          <p className="">Design</p>
-          <p className="">Development</p>
-          <p className="">Photography</p>
-          <p className="">Music</p>
+        <p className="Title-Section col-md-6">New Courses(BOOKS)</p>
+        <div className="tabs  col-md-6  ">
+          <p className=" me-2">All Courses</p>
+          <p className="">Fiction</p>
+          <p className="">Adventure</p>
+          <p className="">Romance</p>
+          <p className="">Fantasy</p>
           <p>
             <img src={DownWardIcon} alt="icon" className=" " />{' '}
           </p>
@@ -18,13 +18,13 @@ function NewMovie({courses}) {
       </div>
 
       {courses.map(course => (
-        <div className="course-content col-md-3 my-3" key={course.id}>
+        <div className="course-content col-md-2 my-3   " key={course.id}>
           <img
             src={course.cover_image}
             alt="Course-content "
             className="col-12 course-image"
           />
-          <p className="course-title col-md-12"></p>
+          <p className="course-title col-md-12 "></p>
           <div className="features row">
             <p className="">Title: {course.title}</p>
             <p className="">Author: {course.author}</p>
